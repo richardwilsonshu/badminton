72a3b95ed23beb61c2f07a8ed85ea76c56c1f4af
Richard Wilson <richard.wilson.shu@hotmail.co.uk>
2022-08-04 19:46:35 +0100

    Add EloBefore back to EloResult for much easier reporting

--- Badminton/Classes/Models/EloResult.cs
+++ Badminton/Classes/Models/EloResult.cs
@@ -2,11 +2,13 @@
 {
     public class EloResult
     {
+        public int EloBefore { get; set; }
         public Match Match { get; set; }
         public int EloAfter { get; set; }
 
-        public EloResult(Match match, int eloAfter)
+        public EloResult(int eloBefore, Match match, int eloAfter)
         {
+            EloBefore = eloBefore;
             Match = match;
             EloAfter = eloAfter;
         }
--- Badminton/Classes/Models/Session.cs
+++ Badminton/Classes/Models/Session.cs
@@ -80,11 +80,16 @@ namespace Badminton.Classes
 
             if (!match.EloNotAffected)
             {
+                foreach (var player in match.Players)
+                {
+                    player.EloResults.Add(new EloResult(player.Elo, match, -1));
+                }
+
                 EloCalculator.UpdateElo(match);
 
                 foreach (var player in match.Players)
                 {
-                    player.EloResults.Add(new EloResult(match, player.Elo));
+                    player.EloResults.Single(er => er.Match == match).EloAfter = player.Elo;
                 }
             }
 
